446e6f187df4da7f6b3c7aa807b5e237f182e7b1
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-08 11:37:46 -0300

    Update Metros_para_centimetro.py

--- Metros_para_centimetro.py
+++ Metros_para_centimetro.py
@@ -1,4 +1,4 @@
 print("Conversor de metros para centimentros")
 metros = float(input("Um numero: "))
 centimetros = metros * 100.0
-print(f"{metros}m tem {round(centimetros,3)}cm")
+print(f"{metros}m tem {round(centimetros,2)}cm")
