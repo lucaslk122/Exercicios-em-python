52419808d089aa3cd6792687812c0973d52a1dc2
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-08 12:33:05 -0300

    Create Conv_ Celsius_Fahrenheit.py

---
+++ Conv_ Celsius_Fahrenheit.py
@@ -0,0 +1,4 @@
+print("Conversor de temperatura Celsius em Fahrenheit")
+Celsius = float(input("Digite a temperatura em graus Celsius: "))
+Fahrenheit = (9.0*Celsius + 160.0) /5.0
+print(f"{Celsius}ºC é equivalente a {round(Fahrenheit,2)}ºF")
