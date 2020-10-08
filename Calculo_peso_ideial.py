96abf3e33a3f9d89cb8b2ff612d1be59365c843f
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-08 12:59:54 -0300

    Create Calculo_peso_ideial.py

---
+++ Calculo_peso_ideial.py
@@ -0,0 +1,4 @@
+print("Programa que calcula o peso ideia baseado na altura da pessoa")
+Altura = float(input("Digite sua altura em metros: "))
+PesoIdeal = (72.7 *Altura) - 58.0
+print(f"Para uma altura de {Altura}m , o peso ideal é de {round(PesoIdeal,2)}Kg ")
