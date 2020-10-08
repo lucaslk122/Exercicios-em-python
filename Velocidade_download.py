34ef20c547cdf8724f8769c66242bff60842109b
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-08 17:02:36 -0300

    Create Velocidade_download.py

---
+++ Velocidade_download.py
@@ -0,0 +1,7 @@
+import math
+
+print("Este programa retorna estimado de download para um dado tamanho de arquivo e velocidade internet")
+arquivo = float(input("Digite o tamanho do arquivo em MB: "))
+velocidade = float(input("Digite a velocidade da sua internet em mbps: "))
+tempo = (arquivo/velocidade)/60
+print(f"Seu arquivo de {arquivo}Mb vai demorar {round(tempo,2)} minutos")
