6223f6f2a350c1dfcd74369b93d2e4f5228a63ba
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-09 08:35:08 -0300

    Update Exe17_loja_tintas_OutrosParametros.py

--- Exe17_loja_tintas_OutrosParametros.py
+++ Exe17_loja_tintas_OutrosParametros.py
@@ -4,12 +4,12 @@ print("Um galão com 18L custa R$80.00 e uma lata com 3,6L por R$25.00")
 Area = float(input("Digite o tamanho da area em metros quadrados a ser pintada: "))
 Litros = Area/6.0
 folga = Litros + (Litros*0.10)
-Latas_80L = math.ceil(folga/18.0)
-Preço_80L = round(Latas_80L,2)*80
+Latas_18L = math.ceil(folga/18.0)
+Preço_18L = round(Latas_18L,2)*80
 Latas_3_6L = math.ceil(folga/3.6)
 Preço_3_6L = round(Latas_3_6L,2)*25
 mistura = (folga//18)*80 + math.ceil((folga%18)//3.6)*25
-print(f"Voce vai precisar de {Latas_80L} lata de tinta de 18L para pintar {Area}m^2, portanto, pagará R${Preço_80L}")
+print(f"Voce vai precisar de {Latas_18L} lata de tinta de 18L para pintar {Area}m^2, portanto, pagará R${Preço_18L}")
 print(f"Voce vai precisar de {Latas_3_6L} lata de tinta de 3.6L para pintar {Area}m^2, portanto, pagará R${Preço_3_6L}")
 print(f"A mistura para o menor valor é R${mistura}")
 
