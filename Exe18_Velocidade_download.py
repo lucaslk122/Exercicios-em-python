a6a317d801f5ed2eea7b5c757e9056ea11858f93
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-09 08:01:42 -0300

    Update Exe18_Velocidade_download.py

--- Exe18_Velocidade_download.py
+++ Exe18_Velocidade_download.py
@@ -3,5 +3,5 @@ import math
 print("Este programa retorna o tepo estimado de download para um dado tamanho de arquivo e velocidade internet")
 arquivo = float(input("Digite o tamanho do arquivo em MB: "))
 velocidade = float(input("Digite a velocidade da sua internet em mbps: "))
-tempo = ((arquivo/8)/velocidade)/60
+tempo = ((arquivo/8.0)/velocidade)/60.0
 print(f"Seu arquivo de {arquivo}Mb vai demorar {round(tempo,2)} minutos")
